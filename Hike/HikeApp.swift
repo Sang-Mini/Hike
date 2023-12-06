005f68224a9f78eb85cebc17b57d933fc78629e3
Sang-Mini <sangmin3062@gmail.com>
2023-12-06 16:44:04 +0900

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by 유상민 on 2023/12/06.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
